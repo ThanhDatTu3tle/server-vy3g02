3a119ca9262fbcddd682822b979ff868242520fd
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2022-05-19 19:01:49 +0700

    push payment

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,10 +4,10 @@ import { SqlServerConnectionOptions } from 'typeorm/driver/sqlserver/SqlServerCo
 const config: SqlServerConnectionOptions = {
   type: 'mssql',
   host: 'localhost',
-  port: 1444,
+  port: 1434, 
   username: 'sa',
-  password: '!dat27032001',
-  database: 'server_vy3_g02',
+  password: 'dat27032001',
+  database: 'server_flight',
   // entities: ['dist/**/*.entity{.ts,.js}'],
   entities: ['dist/output/entities/*js'], 
   synchronize: true,
